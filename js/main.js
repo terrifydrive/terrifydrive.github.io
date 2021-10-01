42ee27ec7153abe7f4467fe6580e774e33d44a5b
terrifydrive <terrifydrive@gmail.com>
2021-10-01 14:21:44 +0300

    *

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
+
 $(document).ready(function () {
-    console.log("ready!");
-
-    let items = document.querySelectorAll('.carousel .carousel-item');
-
-    items.forEach((el) => {
-        const minPerSlide = 3;
-        let next = el.nextElementSibling;
-        for (var i = 1; i < minPerSlide; i++) {
-            if (!next) {
-                // wrap carousel by using first child
-                next = items[0];
-            }
-            let cloneChild = next.cloneNode(true);
-            el.appendChild(cloneChild.children[0]);
-            next = next.nextElementSibling;
-        };
+    $('.releases-carousel').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerMode: false
     });
 });
