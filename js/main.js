85fad3a8ce17349c4ace313327d42499baad7d0b
terrifydrive <terrifydrive@gmail.com>
2021-11-11 11:59:44 +0300

    *

--- js/main.js
+++ js/main.js
@@ -4,22 +4,22 @@ $(document).ready(function () {
     $('.releases-carousel').slick({
         dots: true,
         infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         centerMode: false,
         responsive: [
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                 }
             },
             {
                 breakpoint: 576,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             }
         ]
