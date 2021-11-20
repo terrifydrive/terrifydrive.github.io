25d78df767743557f214e0e27a93519f6c11f06a
terrifydrive <terrifydrive@gmail.com>
2021-11-20 21:49:34 +0300

    *

--- js/main.js
+++ js/main.js
@@ -42,31 +42,31 @@ $(document).ready(function () {
         }
     };
 
-    var fbAddedflag = false;
-    var fbWidth = 0;
-    function fbFeedInit() {
-        var stopNext = false;
-        if (fbAddedflag != true) {
-            fbWidth = parseInt($("#fb-feed-custom-container").width());
-            if (fbWidth > 100) {
-                if (fbWidth > 500)
-                    fbWidth = 500;
-                $("#fb-feed-custom-container").append('<div id="fb-root"></div>');
-                $("#fb-feed-custom-container").append('<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0" nonce="uajXX1YK"></script>');
-                $("#fb-feed-custom-container").append('<div class="fb-page" data-href="https://www.facebook.com/terrifydrive/" data-tabs="timeline" data-width="' + fbWidth + '" data-height="800" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"><blockquote cite="https://www.facebook.com/terrifydrive/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/terrifydrive/">Terrify Drive</a></blockquote></div>');
-                fbAddedflag = true;
-            };
-        } else {
-            var fbHeader = $("div.userContentWrapper");
-            if (fbHeader) {
-                fbHeader.attr("style","display: none;");
-                stopNext = true;
-                console.log("*");
-            };
-        };
-        if (stopNext != true)
-            setTimeout(fbFeedInit, 100);
-    };
-    setTimeout(fbFeedInit, 100);
+    //var fbAddedflag = false;
+    //var fbWidth = 0;
+    //function fbFeedInit() {
+    //    var stopNext = false;
+    //    if (fbAddedflag != true) {
+    //        fbWidth = parseInt($("#fb-feed-custom-container").width());
+    //        if (fbWidth > 100) {
+    //            if (fbWidth > 500)
+    //                fbWidth = 500;
+    //            $("#fb-feed-custom-container").append('<div id="fb-root"></div>');
+    //            $("#fb-feed-custom-container").append('<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0" nonce="uajXX1YK"></script>');
+    //            $("#fb-feed-custom-container").append('<div class="fb-page" data-href="https://www.facebook.com/terrifydrive/" data-tabs="timeline" data-width="' + fbWidth + '" data-height="800" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"><blockquote cite="https://www.facebook.com/terrifydrive/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/terrifydrive/">Terrify Drive</a></blockquote></div>');
+    //            fbAddedflag = true;
+    //        };
+    //    } else {
+    //        var fbHeader = $("div.userContentWrapper");
+    //        if (fbHeader) {
+    //            fbHeader.attr("style","display: none;");
+    //            stopNext = true;
+    //            console.log("*");
+    //        };
+    //    };
+    //    if (stopNext != true)
+    //        setTimeout(fbFeedInit, 100);
+    //};
+    //setTimeout(fbFeedInit, 100);
 
 });
