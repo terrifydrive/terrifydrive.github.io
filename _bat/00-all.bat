44f928edca344d14980d0450c04a0a0fc7e81987
terrifydrive <terrifydrive@gmail.com>
2022-01-14 12:39:02 +0300

    *

--- _bat/00-all.bat
+++
@@ -1,9 +0,0 @@
-
-set gitServerUrl="https://github.com"
-
-call 01-clone
-call 02-angular-packages
-call 03-build-app
-call 04-publish
-
-PAUSE
--- _bat/run.bat
+++
@@ -1,7 +0,0 @@
-echo.
-echo --------------------------------------------------------------------------
-echo RUN...
-
-cd ./terrifydrive.github.io/_app_dev/
-ng serve --open
-cd ./../../
