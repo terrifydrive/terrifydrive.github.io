e5e799eda53e25f694463ebd36d191c47ada7163
alex_k <kononovich.alexandr@gmail.com>
2022-02-20 22:03:42 +0300

    *

--- _app_dev/src/app/services/common-config.service.ts
+++ _app_dev/src/app/services/common-config.service.ts
@@ -5,16 +5,17 @@ export class CommonConfigService {
         artistStartYear: 2021,
         email: "terrifydrive@gmail.com",
         pageVK: "https://vk.com/terrifydrive",
-        pageYoutube: "https://www.youtube.com/channel/UCsIHFfuly9ZMk4y7xbc2k4A",
+        pageYouTube: "https://www.youtube.com/channel/UCsIHFfuly9ZMk4y7xbc2k4A",
         pageFacebook: "https://www.facebook.com/terrifydrive",
         pageInstagram: "https://www.instagram.com/terrifydrive",
+        pageTikTok: "https://www.tiktok.com/@terrifydrive",
         serviceAppleMusic: "https://music.apple.com/artist/terrify-drive/1595354173",
         serviceSpotify: "https://open.spotify.com/artist/4rMXPcSc4q7tXsmKbcnjUQ",
         serviceYandexMusic: "https://music.yandex.ru/artist/13814214",
         serviceVKMusic: "https://share.boom.ru/artist/19605358/?share_auth=02170711580013a692811a619b5c10",
         serviceDeezer: "https://www.deezer.com/artist/151373222",
-        serviceYoutubeMusic: "https://music.youtube.com/channel/UCIBR4vdNMpQJPsTLQ8ZPwtQ",
-        serviceSoundcloud: "https://soundcloud.com/terrifydrive"
+        serviceYouTubeMusic: "https://music.youtube.com/channel/UCIBR4vdNMpQJPsTLQ8ZPwtQ",
+        serviceSoundCloud: "https://soundcloud.com/terrifydrive"
     };
 
     getCommonConfig(): any {
